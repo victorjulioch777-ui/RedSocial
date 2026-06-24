22b8dedf17737eb6a94a741d1cc310bf980d06a2
victor <victorjulioch777@gmail.com>
2026-06-23 18:24:21 -0600

    refactor: reorganize frontend files into html, js and css folders #3

--- public/js/posts.js
+++ public/js/posts.js
@@ -3,15 +3,14 @@ const postList = document.getElementById("postList");
 let posts = JSON.parse(localStorage.getItem("posts")) || [];
 
 if (posts.length === 0) {
-  postList.innerHTML = "<p>No hay posts todavía.</p>";
+  postList.innerHTML = "<p class='empty-state'>No hay posts todavía.</p>";
 } else {
   posts.forEach(function (post) {
     postList.innerHTML += `
       <div class="post">
-        <strong>${post.writer}</strong><br />
+        <strong>${post.writer}</strong>
         <span>${post.content}</span>
       </div>
-      <hr />
     `;
   });
 }
